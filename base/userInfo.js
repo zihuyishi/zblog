b3aa66d14222f998e08cd4926107faf4a91641b6
lichong <lc@molasync.com>
2015-07-07 18:13:24 +0800

    login的逻辑

--- base/userInfo.js
+++ base/userInfo.js
@@ -3,8 +3,21 @@
  */
 var userInfo = function () {
     var user = {},
-        email, username,
-        id, password;
+        m_email, m_username,
+        m_id, m_password;
+
+    user.setUsername = function (name) {
+        m_username = name;
+    };
+    user.getUsername = function () {
+        return m_username;
+    };
+    user.setPassword = function (password) {
+        m_password = password;
+    };
+    user.getPassword = function () {
+        return m_password;
+    };
 
     return user;
 };
--- routes/login.js
+++ routes/login.js
@@ -4,9 +4,16 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../base/config');
+var userinfo = require('../base/userInfo');
 
 router.get('/', function (req, res, next) {
-   res.send("this is login page");
+    res.render('login', {});
+});
+
+router.post('/', function (req, res, next) {
+    var user = userinfo();
+    user.setUsername(req.body.username);
+    user.setPassword(req.body.password);
 });
 
 
