11796c9cfe5e4c3aa412f331bd0e14fe9821fa6f
lichong <lc@molasync.com>
2015-07-09 13:47:41 +0800

    用户查询

--- database/db_users.js
+++ database/db_users.js
@@ -2,3 +2,47 @@
  * Created by lichong on 15/7/7.
  */
 
+var mongoClient = require('mongodb').MongoClient;
+var config = require('../base/config');
+var userInfo = require('../base/userInfo');
+
+var url = config.db_url;
+
+var db_user = {
+
+};
+
+function _findUserByCondition(condition, callback) {
+    mongoClient.connect(url, function (err, db) {
+        if (err == null) {
+            db.collection('users')
+                .findOne(condition, function (err, doc) {
+                    db.close();
+                    if (err == null && doc != null) {
+                        var user = new userInfo();
+                        user.fromJsonObj(doc);
+                        callback(err, user);
+                    } else {
+                        callback(err);
+                    }
+                });
+        } else {
+            callback(err);
+        }
+    });
+}
+
+db_user.findUserByName = function (username, callback) {
+    _findUserByCondition({"username": username}, callback);
+};
+
+db_user.findUserById = function (userId, callback) {
+    _findUserByCondition({"id": Number(userId)}, callback);
+};
+
+db_user.findUserByEmail = function (email, callback) {
+    _findUserByCondition({"email": email}, callback);
+};
+
+
+module.exports = db_user;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var db_users = require('../database/db_users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res, next) {
+  db_users.findUserById(req.params.id, function (err, user) {
+    if (err == null && user != null) {
+      res.send(user);
+    } else {
+      res.send("user not found");
+    }
+  });
 });
 
 module.exports = router;
